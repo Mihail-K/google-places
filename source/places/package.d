7646e6448fed087c88c697dd2fbb834963ae6c24
Mihail-K <mihail@platterz.ca>
2017-03-17 09:13:56 -0400

    Add package file.

---
+++ source/places/package.d
@@ -0,0 +1,16 @@
+
+module places;
+
+public import places.address_component;
+public import places.aspect_rating;
+public import places.coordinate;
+public import places.day_time;
+public import places.detail;
+public import places.geometry;
+public import places.json_data;
+public import places.opening_hour;
+public import places.period;
+public import places.photo;
+public import places.review;
+public import places.util;
+public import places.viewport;
