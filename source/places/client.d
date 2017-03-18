3e52db9d56d14a0975deb9e7f78735d9fb76c47b
Mihail-K <mihail@platterz.ca>
2017-03-18 15:34:23 -0400

    Add different search types.

--- source/places/client.d
+++ source/places/client.d
@@ -12,8 +12,10 @@ struct Client
 private:
     string _key;
 
-    enum detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json";
-    enum searchUrl  = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+    enum detailsUrl      = "https://maps.googleapis.com/maps/api/place/details/json";
+    enum nearbySearchUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+    enum radarSearchUrl  = "https://maps.googleapis.com/maps/api/place/radarsearch/json";
+    enum textSearchUrl   = "https://maps.googleapis.com/maps/api/place/textsearch/json";
 
 public:
     this(string key)
@@ -27,18 +29,18 @@ public:
         return _key;
     }
 
-    const(Details) details(string placeID)
+    const(Details) details(string placeID) const
     {
         return details(["place_id": placeID]);
     }
 
-    const(Details) details(string[string] options)
+    const(Details) details(string[string] options) const
     {
         auto response = requestHTTP(buildUrl(detailsUrl, addKeyToOptions(options)));
         return Details(response.readJson);
     }
 
-    void details(string[string] options, scope void delegate(const(Details)) callback)
+    void details(string[string] options, scope void delegate(const(Details)) callback) const
     {
         requestHTTP(
             buildUrl(detailsUrl, addKeyToOptions(options)),
@@ -47,22 +49,37 @@ public:
         );
     }
 
-    const(Search) search(string[string] options)
+    const(Search) nearbySearch(string[string] options) const
     {
-        auto response = requestHTTP(buildUrl(searchUrl, addKeyToOptions(options)));
-        return Search(response.readJson);
+        return search(nearbySearchUrl, options);
     }
 
-    void search(string[string] options, scope void delegate(const(Search)) callback)
+    void nearbySearch(string[string] options, scope void delegate(const(Search)) callback) const
     {
-        requestHTTP(
-            buildUrl(searchUrl, addKeyToOptions(options)),
-            (scope request)  { /+ Nothing +/ },
-            (scope response) { callback(Search(response.readJson)); }
-        );
+        return search(nearbySearchUrl, options, callback);
     }
 
-    private string[string] addKeyToOptions(string[string] options)
+    const(Search) radarSearch(string[string] options) const
+    {
+        return search(radarSearchUrl, options);
+    }
+
+    void radarSearch(string[string] options, scope void delegate(const(Search)) callback) const
+    {
+        return search(radarSearchUrl, options, callback);
+    }
+
+    const(Search) textSearch(string[string] options) const
+    {
+        return search(textSearchUrl, options);
+    }
+
+    void textSearch(string[string] options, scope void delegate(const(Search)) callback) const
+    {
+        return search(textSearchUrl, options, callback);
+    }
+
+    private string[string] addKeyToOptions(string[string] options) const
     {
         if(options !is null && "key" !in options)
         {
@@ -70,5 +87,20 @@ public:
         }
 
         return options;
+    }
+
+    private const(Search) search(string url, string[string] options) const
+    {
+        auto response = requestHTTP(buildUrl(url, addKeyToOptions(options)));
+        return Search(response.readJson);
+    }
+
+    private void search(string url, string[string] options, scope void delegate(const(Search)) callback) const
+    {
+        requestHTTP(
+            buildUrl(url, addKeyToOptions(options)),
+            (scope request)  { /+ Nothing +/ },
+            (scope response) { callback(Search(response.readJson)); }
+        );
     }
 }
